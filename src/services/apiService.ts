13e62b3f3997d73210dff5d964d893be78ccb6b2
Mateus Nunes <mateusnss@protonmail.ch>
2024-09-29 09:31:39 -0300

    uwu

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://httpterod-ctilo-production.up.railway.app';
 
 // Função existente para criar uma carta
 export const createLetter = async (data: any) => {
